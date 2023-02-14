aa1c02d2d82af250356a914c9e18e2e38c9243e1
dhramvir73 <dharmvirkumar7321@gmail.com>
2023-02-14 20:40:45 +0530

    setHeader Added

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ import usersRoute from './routes/users.js'
 import hotelsRoute from './routes/hotels.js'
 import roomsRoute from './routes/rooms.js'
 import cookieParser from 'cookie-parser';
+
 const app = express();
 dotenv.config();
+
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
 
 const connect = async () => {
